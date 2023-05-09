22495bdd1222620b0b7c5797e974fdd8aac0131d
Banibro-IT-Solutions <48946673+Banibro-IT-Solutions@users.noreply.github.com>
2023-05-09 09:40:29 +0530

    Add files via upload
    This Module helps to Create counterpart Receipt/Delivery Orders between companies.

---
+++ br_stock_intercompany_transfer/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'Inter Company Stock Transfer',
+    'version': '13.0.1.0.0',
+    'summary': """Create counterpart Receipt/Delivery Orders between companies.""",
+    'description': """Automatically Create Receipt/Delivery orders if any company validates a 
+                      Deliver Order/Receipt to the selected company,Inter Company Stock Transfer, Stock Transfer,
+                      Create counterpart Receipt/Delivery Orders between companies""",
+    'category': 'Warehouse',
+    'author': 'Banibro IT Solutions Pvt Ltd.',
+    'company': 'Banibro IT Solutions Pvt Ltd.',
+    'website': 'https://banibro.com',
+    'depends': ['stock', 'account'],
+    'data': [
+        'views/res_company_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'email': "support@banibro.com",
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
