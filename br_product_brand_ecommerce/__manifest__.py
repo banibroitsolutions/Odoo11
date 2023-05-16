98a1832da19d2ae89e02ec2a332e5b2637ee4685
Banibro-IT-Solutions <48946673+Banibro-IT-Solutions@users.noreply.github.com>
2023-05-16 13:41:30 +0530

    Add files via upload
    This Module Helps to view the Product Brand in eCommerce

---
+++ br_product_brand_ecommerce/__manifest__.py
@@ -0,0 +1,23 @@
+
+
+{
+    'name': 'Product Brand in eCommerce',
+    'version': '13.0.1.1.0',
+    'category': 'eCommerce',
+    'summary': 'Product Brand in eCommerce',
+    'description': 'Product Brand in eCommerce',
+    'author': 'Banibro IT Solutions Pvt Ltd.',
+    'company': 'Banibro IT Solutions Pvt Ltd.',
+    'website': 'https://banibro.com',
+    'images': ['static/description/banner.png'],
+    'depends': ['br_product_brand_sale','website_sale'],
+    'data': [
+        'views/template.xml',
+    ],
+    'license': 'AGPL-3',
+    'email': "support@banibro.com",
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
